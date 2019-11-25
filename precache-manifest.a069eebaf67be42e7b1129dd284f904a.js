dfb76fd7fc032ac9cf29108b3e58792669e75656
Vivych Oksana <vivichokcana@gmail.com>
2019-11-26 00:00:38 +0200

    Updates

---
+++ precache-manifest.a069eebaf67be42e7b1129dd284f904a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f84eeca01b3d5e4f8df964178d22f3",
+    "url": "/vitamins/index.html"
+  },
+  {
+    "revision": "00ec9c98c0a7ae3d741b",
+    "url": "/vitamins/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4c051acb00f768b921b0",
+    "url": "/vitamins/static/js/2.89c5d4b2.chunk.js"
+  },
+  {
+    "revision": "00ec9c98c0a7ae3d741b",
+    "url": "/vitamins/static/js/main.68e9599c.chunk.js"
+  },
+  {
+    "revision": "e9b6804f6fe51f4ba5c1",
+    "url": "/vitamins/static/js/runtime-main.ac82214c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vitamins/static/media/logo.25bf045c.svg"
+  }
+]);
